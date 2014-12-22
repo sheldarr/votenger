9f9668c8908814fb1dba5ba3241d6fe4e9ddfeaa
Sheldar <kewin.polok@gmail.com>
2014-12-22 17:52:19 +0100

    Self-hosting nancy bootstrapper

---
+++ Votenger/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace Votenger
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
---
+++ Votenger/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Votenger
+{
+    using System;
+    using Nancy.Hosting.Self;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = new Uri("http://localhost:6699");
+
+            var hostConfiguration = new HostConfiguration
+            {
+                UrlReservations = new UrlReservations { CreateAutomatically = true }
+            };
+
+            using (var host = new NancyHost(hostConfiguration, uri))
+            {
+                host.Start();
+
+                Console.WriteLine("Your application is running on " + uri);
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
