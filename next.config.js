722c8c03e44d13d1c13765240b0e3a7446885d49
sheldar <kewin.polok@gmail.com>
2019-10-21 21:11:01 +0200

    Basic votes api

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+
+module.exports = {
+  env: {
+    APP_PORT: process.env.APP_PORT,
+    APP_DOMAIN: process.env.APP_DOMAIN,
+    APP_PROTOCOL: process.env.APP_PROTOCOL,
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
+import axios from 'axios';
+import { NextPage } from 'next';
 import React from 'react';
 
-const Home = () => <>Votenger 3.0</>;
+interface Props {
+  votes: Vote[];
+}
+
+const Home: NextPage<Props> = ({ votes }: Props) => (
+  <main>Votes: {JSON.stringify(votes)}</main>
+);
+
+interface Vote {
+  name: string;
+}
+
+Home.getInitialProps = async () => {
+  const { data: votes } = await axios.get<Vote[]>(
+    `${process.env.APP_PROTOCOL}://${process.env.APP_DOMAIN}:${process.env.APP_PORT}/api/votes`,
+  );
+
+  return {
+    votes,
+  };
+};
 
 export default Home;
