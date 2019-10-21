fc016ee515c94ba61ad262318e587ca726135053
sheldar <kewin.polok@gmail.com>
2019-10-21 21:20:45 +0200

    Basic votes api

--- pages/api/votes/repository/index.ts
+++ pages/api/votes/repository/index.ts
@@ -1,7 +1,7 @@
 import low from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 
-const adapter = new FileSync('db.json');
+const adapter = new FileSync('db/db.json');
 const db = low(adapter);
 
 db.defaults({ votes: [] }).write();
