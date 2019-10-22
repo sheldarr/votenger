0eb7cfdb1c363aa7bb24466806355e0768d412a7
sheldar <kewin.polok@gmail.com>
2019-10-22 18:37:39 +0200

    Add material-ui dependency

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class VotengerDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default VotengerDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import axios from 'axios';
 import { NextPage } from 'next';
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
 interface Props {
   votes: Vote[];
 }
 
 const Home: NextPage<Props> = ({ votes }: Props) => (
-  <main>Votes: {JSON.stringify(votes)}</main>
+  <Box>
+    <main>Votes: {JSON.stringify(votes)}</main>
+    <Button variant="contained" color="primary">
+      Create Vote
+    </Button>
+  </Box>
 );
 
 interface Vote {
