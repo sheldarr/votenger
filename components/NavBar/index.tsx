70064f9946b6fed2c39976bbffcff6f89519247a
sheldar <kewin.polok@gmail.com>
2019-12-09 20:07:27 +0100

    Username in Navbar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+
+import { getUsername } from '../../auth';
+
+const Logo = styled.div`
+  flex-grow: 1;
+`;
 
 const NavBar = () => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const currentUsername = getUsername();
+
+    setUsername(currentUsername);
+  }, []);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">Votenger</Typography>
+        <Logo>
+          <Typography variant="h6">Votenger</Typography>
+        </Logo>
+        <Typography variant="h6">{username}</Typography>
       </Toolbar>
     </AppBar>
   );
