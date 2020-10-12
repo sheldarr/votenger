0fe61bb8a205cc40c0ca372a5390976f5e8215dd
Krzysztof Krzyrok Stachyra <krzysztof.stachyra@shijigroup.com>
2020-10-12 20:07:51 +0200

    configure jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+};
