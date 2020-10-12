83407969ca1f798e5a6bfd1ca910e289e915751c
Krzysztof Krzyrok Stachyra <krzysztof.stachyra@shijigroup.com>
2020-10-12 20:29:48 +0200

    add additional test if random returns 0

--- utils/weightedRandomGame.test.ts
+++ utils/weightedRandomGame.test.ts
@@ -2,6 +2,20 @@ import 'jest';
 import { weightedRandomGame } from './weightedRandomGame';
 
 describe('weightedRandomGame', () => {
+  test('returns 1st game if all games have the same number of votes and random returns 0', () => {
+    const games = {
+      '1Game': ['paczek'],
+      '2Game': ['kaczek'],
+      '3Game': ['laczek'],
+      '4Game': ['baczek'],
+    };
+    Math.random = () => 0;
+
+    const result = weightedRandomGame(games);
+
+    expect(result).toEqual('1Game');
+  });
+
   test('returns 3rd game if all games have the same number of votes and random returns the lowest number for 3rd game', () => {
     const games = {
       '1Game': ['paczek'],
@@ -30,32 +44,18 @@ describe('weightedRandomGame', () => {
     expect(result).toEqual('3Game');
   });
 
-  test('returns 3rd game if all games have no votes and random returns number for 3rd range', () => {
+  test('returns game if it is the only game with the votes', () => {
     const games = {
       '1Game': [],
       '2Game': [],
-      '3Game': [],
-      '4Game': [],
-    };
-    Math.random = () => 0.6;
-
-    const result = weightedRandomGame(games);
-
-    expect(result).toEqual('3Game');
-  });
-
-  test('returns 2nd game if it is the only game with the votes', () => {
-    const games = {
-      '1Game': [],
-      '2Game': ['paczek'],
-      '3Game': [],
+      '3Game': ['paczek'],
       '4Game': [],
     };
     Math.random = () => 0;
 
     const result = weightedRandomGame(games);
 
-    expect(result).toEqual('2Game');
+    expect(result).toEqual('3Game');
   });
 
   test('returns 2nd game if it has the most votes and if random returns the lowest number for it', () => {
