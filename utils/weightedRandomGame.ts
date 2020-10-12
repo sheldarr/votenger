a34838fccfa398ae2aaf75bfc0a94bc2edc6b292
Krzysztof Krzyrok Stachyra <krzysztof.stachyra@shijigroup.com>
2020-10-12 20:57:18 +0200

    refactor weightedRandomGame
    multiply games with votes, simplify code

--- utils/weightedRandomGame.ts
+++ utils/weightedRandomGame.ts
@@ -1,26 +1,9 @@
 export const weightedRandomGame = (games: Record<string, string[]>) => {
-  const randomNumber = Math.random();
-  const numberOfGames = Object.keys(games).length;
-  const allVotesNumber = Object.values(games).reduce(
-    (votesSum, peopleVotedOnGame) => votesSum + peopleVotedOnGame.length,
-    0,
+  const gamesToPick = Object.entries(
+    games,
+  ).flatMap(([gameName, peopleVotedOnGame]) =>
+    peopleVotedOnGame.map((_) => gameName),
   );
-  const rangeForOneVote = 1 / (allVotesNumber || numberOfGames);
-
-  let gameLowestNumber = 0;
-  for (const name in games) {
-    const votesNumberForGame = games[name].length;
-    const gameHighestNumber =
-      gameLowestNumber + votesNumberForGame * rangeForOneVote;
-    if (randomNumber < gameHighestNumber) {
-      console.log(
-        `gameHighestNumber: ${gameHighestNumber}; name: ${name}; random: ${randomNumber}`,
-      );
-      return name;
-    }
-
-    gameLowestNumber = gameHighestNumber;
-  }
-
-  throw new Error('Your algorithm of drawing the game sucks!!!');
+  const gameIndex = Math.floor(Math.random() * gamesToPick.length);
+  return gamesToPick[gameIndex];
 };
