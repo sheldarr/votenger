6a4ccf6e402cca7fc25a315fe382985d28201a97
Krzysztof Krzyrok Stachyra <krzysztof.stachyra@shijigroup.com>
2020-10-12 21:05:27 +0200

    remove unnecessary parameter in arrow function

--- utils/weightedRandomGame.ts
+++ utils/weightedRandomGame.ts
@@ -2,7 +2,7 @@ export const weightedRandomGame = (games: Record<string, string[]>) => {
   const gamesToPick = Object.entries(
     games,
   ).flatMap(([gameName, peopleVotedOnGame]) =>
-    peopleVotedOnGame.map((_) => gameName),
+    peopleVotedOnGame.map(() => gameName),
   );
   const gameIndex = Math.floor(Math.random() * gamesToPick.length);
   return gamesToPick[gameIndex];
