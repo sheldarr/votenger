92214078e1e2400a722c5eff27767e81a106265d
sheldar <kewin.polok@gmail.com>
2020-10-26 20:20:01 +0100

    Remove custom jest config

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-  },
-};
