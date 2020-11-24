6dfb13ced46a1ee273843f08dcf7a8784bd40e0a
sheldar <kewin.polok@gmail.com>
2020-11-24 19:29:20 +0100

    GameState enum

--- pages/api/games/index.ts
+++ pages/api/games/index.ts
@@ -11,58 +11,76 @@ export enum GameType {
   SURVIVAL = 'Survival',
 }
 
+export enum GameState {
+  AVAILABLE = 'Available',
+  EXCLUDED = 'Excluded',
+}
+
 export interface Game {
   name: string;
-  type: string;
+  state: GameState;
+  type: GameType;
 }
 
 const GAMES: Game[] = [
   {
     name: 'Alien vs Predator 2',
+    state: GameState.AVAILABLE,
     type: GameType.FPS,
   },
   {
     name: 'Among us',
+    state: GameState.AVAILABLE,
     type: GameType.SURVIVAL,
   },
   {
     name: 'Battle for the Middle Earth 2',
+    state: GameState.AVAILABLE,
     type: GameType.RTS,
   },
   {
     name: 'CS 1.6',
+    state: GameState.AVAILABLE,
     type: GameType.FPS,
   },
   {
     name: 'Diablo II',
+    state: GameState.AVAILABLE,
     type: GameType.HACK_AND_SLASH,
   },
   {
     name: 'ET: Legacy',
+    state: GameState.AVAILABLE,
     type: GameType.FPS,
   },
   {
     name: 'Mount & Blade',
+    state: GameState.AVAILABLE,
     type: GameType.ACTION,
   },
   {
     name: 'Re-Volt',
+    state: GameState.AVAILABLE,
     type: GameType.RACING,
   },
   {
     name: 'Settlers 3',
+    state: GameState.AVAILABLE,
     type: GameType.RTS,
   },
   {
     name: 'Soldat',
+    state: GameState.AVAILABLE,
     type: GameType.ACTION,
   },
   {
     name: 'Stronghold: Crusader',
+    state: GameState.AVAILABLE,
     type: GameType.RTS,
   },
   {
     name: 'Unreal Tournament 2k4',
+    state: GameState.AVAILABLE,
     type: GameType.FPS,
   },
 ];
