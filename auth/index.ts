402cd69b8b3ef9dc469ca580da2cd0cf7c9b807c
sheldar <kewin.polok@gmail.com>
2020-11-26 20:06:31 +0100

    Change admins separator

--- auth/index.ts
+++ auth/index.ts
@@ -1,4 +1,4 @@
-const ADMINS_SEPARATOR = ';';
+const ADMINS_SEPARATOR = ',';
 
 export const isUserAdmin = (username: string) => {
   return process.env.NEXT_PUBLIC_ADMINS.split(ADMINS_SEPARATOR).includes(
