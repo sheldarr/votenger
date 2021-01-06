9a4a842d4e3a1440921d9f678604344b685f5286
sheldar <kewin.polok@gmail.com>
2021-01-06 13:18:19 +0100

    usePolls autorefresh

--- hooks/usePolls/index.ts
+++ hooks/usePolls/index.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
 import useSWR from 'swr';
+import { WebSocketEvents } from '../../events';
 import { Poll } from '../../getDb/polls';
+import useSocket from '../useSocket';
 
 const fetcher = (url: string) =>
   axios.get<Poll[]>(url).then(({ data }) => data);
 
-const usePolls = () => useSWR('/api/polls', fetcher);
+const usePolls = () => {
+  const response = useSWR('/api/polls', fetcher);
+
+  useSocket(WebSocketEvents.REFRESH_POLLS, () => {
+    response.mutate();
+  });
+
+  return response;
+};
 
 export default usePolls;
