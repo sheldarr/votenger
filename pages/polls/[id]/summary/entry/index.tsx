4e5d269c2b6b49e3b7f03bc7d79354201a2fa794
sheldar <kewin.polok@gmail.com>
2021-01-14 20:55:01 +0100

    Games propositions

--- pages/polls/[id]/summary/entry/index.tsx
+++ pages/polls/[id]/summary/entry/index.tsx
@@ -11,6 +11,7 @@ import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import CheckIcon from '@material-ui/icons/Check';
 import update from 'immutability-helper';
+import TextField from '@material-ui/core/TextField';
 
 import useGames from '../../../../../hooks/useGames';
 import usePoll from '../../../../../hooks/usePoll';
@@ -19,6 +20,7 @@ import useUser from '../../../../../hooks/useUser';
 import { URL as SUMMARY_URL } from '..';
 import Page from '../../../../../components/Page';
 import { Decision, GameDecision } from '../../../../../getDb/polls';
+import { Game } from '../../../../../getDb/games';
 
 export const URL = (pollId: string) => `/polls/${pollId}/summary/entry`;
 
@@ -50,22 +52,43 @@ const PollSummaryPage: React.FunctionComponent = () => {
   const { data: poll } = usePoll(router.query.id as string);
   const { data: games } = useGames();
 
-  const [gamesDecisions, setGamesDecisions] = useState<GameDecision[]>([]);
-
-  const playedGames = [
-    ...new Set(poll?.votes.flatMap((vote) => vote.votedFor)),
-  ].map((playedGame) => {
-    return games?.find((game) => game.name === playedGame);
-  });
+  const [playedGames, setPlayedGames] = useState<Game[] | undefined>();
+  const [gamesDecisions, setGamesDecisions] = useState<
+    GameDecision[] | undefined
+  >();
+  const [gamesPropositions, setGamesProposistions] = useState<string[]>(
+    Array(process.env.NEXT_PUBLIC_NUMBER_OF_GAMES_PROPOSITIONS).fill(''),
+  );
 
   useEffect(() => {
-    setGamesDecisions(
-      games?.map((game) => ({
-        decision: 'KEEP',
-        name: game.name,
-      })),
-    );
+    games &&
+      poll?.votes &&
+      setPlayedGames(
+        poll?.alreadyPlayed.map((playedGame) => {
+          return games?.find((game) => game.name === playedGame);
+        }),
+      );
+  }, [games, poll]);
+
+  useEffect(() => {
+    playedGames &&
+      setGamesDecisions(
+        playedGames.map((game) => ({
+          decision: 'KEEP',
+          name: game.name,
+        })),
+      );
   }, [playedGames]);
+
+  const handlePropositionChange = (proposition: string, index: number) => {
+    setGamesProposistions(
+      update(gamesPropositions, {
+        [index]: {
+          $set: proposition,
+        },
+      }),
+    );
+  };
 
   const findGameDecision = (name: string) => {
     return gamesDecisions.find((gameDecision) => gameDecision.name === name);
@@ -91,49 +114,74 @@ const PollSummaryPage: React.FunctionComponent = () => {
         {poll?.name}
       </Typography>
       <Grid container spacing={1}>
-        {playedGames?.map((game) => (
-          <Grid item key={game.name} lg={4} md={6} xs={12}>
-            <GameCard
-              toRemove={findGameDecision(game.name).decision === 'REMOVE'}
-              variant="outlined"
-            >
-              <CardContent>
-                <Typography gutterBottom component="h2" variant="h6">
-                  {game.name}
-                </Typography>
-                <Typography color="textSecondary">{game.type}</Typography>
-              </CardContent>
-              <CardActions>
-                {findGameDecision(game.name).decision === 'KEEP' ? (
-                  <Button
-                    color="secondary"
-                    onClick={() => {
-                      changeDecision(game.name, 'REMOVE');
-                    }}
-                  >
-                    Remove
-                  </Button>
-                ) : (
-                  <Button
-                    color="primary"
-                    onClick={() => {
-                      changeDecision(game.name, 'KEEP');
-                    }}
-                  >
-                    Keep
-                  </Button>
-                )}
-              </CardActions>
-            </GameCard>
-          </Grid>
-        ))}
+        {gamesDecisions &&
+          playedGames.map((game) => (
+            <Grid item key={game.name} lg={4} md={6} xs={12}>
+              <GameCard
+                toRemove={findGameDecision(game.name)?.decision === 'REMOVE'}
+                variant="outlined"
+              >
+                <CardContent>
+                  <Typography gutterBottom component="h2" variant="h6">
+                    {game.name}
+                  </Typography>
+                  <Typography color="textSecondary">{game.type}</Typography>
+                </CardContent>
+                <CardActions>
+                  {findGameDecision(game.name).decision === 'KEEP' ? (
+                    <Button
+                      color="secondary"
+                      onClick={() => {
+                        changeDecision(game.name, 'REMOVE');
+                      }}
+                    >
+                      Remove
+                    </Button>
+                  ) : (
+                    <Button
+                      color="primary"
+                      onClick={() => {
+                        changeDecision(game.name, 'KEEP');
+                      }}
+                    >
+                      Keep
+                    </Button>
+                  )}
+                </CardActions>
+              </GameCard>
+            </Grid>
+          ))}
+      </Grid>
+      <Grid container spacing={1}>
+        <Grid item>
+          <Typography gutterBottom component="h2" variant="h4">
+            New propositions
+          </Typography>
+        </Grid>
+        <Grid container item spacing={1}>
+          {gamesPropositions.map((proposition, index) => (
+            <TextField
+              fullWidth
+              id={`proposition-${index}`}
+              inputProps={{ maxLength: 48 }}
+              key={index}
+              label={`Proposition ${index + 1}`}
+              margin="normal"
+              onChange={(event) =>
+                handlePropositionChange(event.target.value, index)
+              }
+              value={proposition}
+            />
+          ))}
+        </Grid>
       </Grid>
       <SummarizeFab
         color="primary"
+        disabled={gamesPropositions?.some((proposition) => !proposition)}
         onClick={async () => {
           await axios.post(`/api/polls/${router.query.id}/summary/entry`, {
             gamesDecisions,
-            proposedGames: [],
+            proposedGames: gamesPropositions,
             username: user?.username,
           });
 
