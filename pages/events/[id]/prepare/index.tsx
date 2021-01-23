03a1aaef7e26418868f4db642b2ba66e0b6daf1f
sheldar <kewin.polok@gmail.com>
2021-01-23 14:31:03 +0100

    Update prepare page layout

--- pages/events/[id]/prepare/index.tsx
+++ pages/events/[id]/prepare/index.tsx
@@ -96,177 +96,203 @@ const PrepareEventPage: React.FunctionComponent = () => {
 
   return (
     <Page title={`${event?.name} preparation`}>
-      <Typography gutterBottom align="center" variant="h4">
-        {event?.name} {event?.preparation.appliedAt && '(applied)'}
-      </Typography>
-      <Grid container spacing={1}>
-        <Grid container item justify="space-between" spacing={1} xs={12}>
-          <Grid item>
-            <Grid container spacing={1}>
-              {players
-                ?.sort((a, b) => a.localeCompare(b))
-                .map((player) => (
-                  <Grid item key={player}>
-                    <Chip
-                      color="primary"
-                      label={player}
-                      variant={
-                        player === user?.username ? 'default' : 'outlined'
-                      }
-                    />
-                  </Grid>
-                ))}
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Chip color="primary" icon={<GroupIcon />} label={players.length} />
-          </Grid>
-        </Grid>
-        <Grid container item spacing={1} xs={12}>
-          <Typography gutterBottom variant="h4">
-            Term
+      <Grid container direction="column" spacing={3}>
+        <Grid item>
+          <Typography gutterBottom align="center" variant="h4">
+            {event?.name} {event?.preparation.appliedAt && '(applied)'}
           </Typography>
-          <Button color="primary" onClick={() => setIsNewTermPickerOpen(true)}>
-            Add term
-          </Button>
-          <DatePicker
-            disablePast
-            TextFieldComponent={() => null}
-            onChange={(term) => addTermProposition(term.toISOString())}
-            onClose={() => setIsNewTermPickerOpen(false)}
-            onOpen={() => setIsNewTermPickerOpen(true)}
-            open={isNewTermPickerOpen}
-            value={undefined}
-          />
         </Grid>
-        {event?.preparation.possibleTerms
-          .sort((a, b) => {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            return new Date(a.date) - new Date(b.date);
-          })
-          .map((possibleTerm) => (
-            <Grid container item key={possibleTerm.date} spacing={1} xs={12}>
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <CustomCheckbox
-                      checked={possibleTerm.usernames.some(
-                        (username) => username === user?.username,
-                      )}
-                      color="primary"
-                      disabled={!!event?.preparation.selectedTerm}
-                      name={possibleTerm.date}
-                      onChange={() => {
-                        switchTerm(possibleTerm.date);
-                      }}
-                      selected={
-                        event?.preparation.selectedTerm === possibleTerm.date
-                      }
-                    />
-                  }
-                  label={format(new Date(possibleTerm.date), 'dd.MM.yyyy')}
-                />
-                {isUserAdmin(user?.username) && (
-                  <Button
-                    color="primary"
-                    disabled={
-                      event?.preparation.selectedTerm &&
-                      possibleTerm.date !== event?.preparation.selectedTerm
-                    }
-                    onClick={() => {
-                      switchSelectedTerm(possibleTerm.date);
-                    }}
-                  >
-                    {event?.preparation.selectedTerm === possibleTerm.date
-                      ? 'Unselect'
-                      : 'Select'}
-                  </Button>
-                )}
+        <Grid item>
+          <Grid container item justify="space-between" spacing={1}>
+            <Grid item>
+              <Grid container spacing={1}>
+                {players
+                  ?.sort((a, b) => a.localeCompare(b))
+                  .map((player) => (
+                    <Grid item key={player}>
+                      <Chip
+                        color="primary"
+                        label={player}
+                        variant={
+                          player === user?.username ? 'default' : 'outlined'
+                        }
+                      />
+                    </Grid>
+                  ))}
               </Grid>
-              {possibleTerm.usernames
-                .sort((a, b) => a.localeCompare(b))
-                .map((username) => (
-                  <Grid item key={username}>
-                    <Chip
-                      color="primary"
-                      label={username}
-                      variant={
-                        username === user?.username ? 'default' : 'outlined'
+            </Grid>
+            <Grid item>
+              <Chip
+                color="primary"
+                icon={<GroupIcon />}
+                label={players.length}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container spacing={1}>
+            <Grid item>
+              <Typography gutterBottom variant="h4">
+                Term
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button
+                color="primary"
+                onClick={() => setIsNewTermPickerOpen(true)}
+              >
+                Add term
+              </Button>
+              <DatePicker
+                disablePast
+                TextFieldComponent={() => null}
+                onChange={(term) => addTermProposition(term.toISOString())}
+                onClose={() => setIsNewTermPickerOpen(false)}
+                onOpen={() => setIsNewTermPickerOpen(true)}
+                open={isNewTermPickerOpen}
+                value={undefined}
+              />
+            </Grid>
+          </Grid>
+          <Grid container direction="column">
+            {event?.preparation.possibleTerms
+              .sort((a, b) => {
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                return new Date(a.date) - new Date(b.date);
+              })
+              .map((possibleTerm) => (
+                <Grid container item key={possibleTerm.date} spacing={1}>
+                  <Grid item>
+                    <FormControlLabel
+                      control={
+                        <CustomCheckbox
+                          checked={possibleTerm.usernames.some(
+                            (username) => username === user?.username,
+                          )}
+                          color="primary"
+                          disabled={!!event?.preparation.selectedTerm}
+                          name={possibleTerm.date}
+                          onChange={() => {
+                            switchTerm(possibleTerm.date);
+                          }}
+                          selected={
+                            event?.preparation.selectedTerm ===
+                            possibleTerm.date
+                          }
+                        />
                       }
+                      label={format(new Date(possibleTerm.date), 'dd.MM.yyyy')}
                     />
                   </Grid>
-                ))}
-            </Grid>
-          ))}
-        <Grid container item spacing={1} xs={12}>
+                  <Grid item>
+                    {isUserAdmin(user?.username) && (
+                      <Button
+                        color="primary"
+                        disabled={
+                          event?.preparation.selectedTerm &&
+                          possibleTerm.date !== event?.preparation.selectedTerm
+                        }
+                        onClick={() => {
+                          switchSelectedTerm(possibleTerm.date);
+                        }}
+                      >
+                        {event?.preparation.selectedTerm === possibleTerm.date
+                          ? 'Unselect'
+                          : 'Select'}
+                      </Button>
+                    )}
+                  </Grid>
+                  {possibleTerm.usernames
+                    .sort((a, b) => a.localeCompare(b))
+                    .map((username) => (
+                      <Grid item key={username}>
+                        <Chip
+                          color="primary"
+                          label={username}
+                          variant={
+                            username === user?.username ? 'default' : 'outlined'
+                          }
+                        />
+                      </Grid>
+                    ))}
+                </Grid>
+              ))}
+          </Grid>
+        </Grid>
+        <Grid item>
           <Typography gutterBottom variant="h4">
             Type
           </Typography>
-        </Grid>
-        {eventTypes &&
-          eventTypes.map((eventType) => (
-            <Grid container item key={eventType} spacing={1} xs={12}>
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <CustomCheckbox
-                      checked={event?.preparation.eventTypeVotes.some(
-                        (eventTypeVote) =>
-                          eventTypeVote.username === user?.username &&
-                          eventTypeVote.type === eventType,
-                      )}
-                      color="primary"
-                      disabled={!!event?.preparation.selectedEventType}
-                      name={eventType}
-                      onChange={() => {
-                        switchEventType(eventType);
-                      }}
-                      selected={
-                        event?.preparation.selectedEventType === eventType
+          <Grid container direction="column">
+            {eventTypes &&
+              eventTypes.map((eventType) => (
+                <Grid container item key={eventType} spacing={1}>
+                  <Grid item>
+                    <FormControlLabel
+                      control={
+                        <CustomCheckbox
+                          checked={event?.preparation.eventTypeVotes.some(
+                            (eventTypeVote) =>
+                              eventTypeVote.username === user?.username &&
+                              eventTypeVote.type === eventType,
+                          )}
+                          color="primary"
+                          disabled={!!event?.preparation.selectedEventType}
+                          name={eventType}
+                          onChange={() => {
+                            switchEventType(eventType);
+                          }}
+                          selected={
+                            event?.preparation.selectedEventType === eventType
+                          }
+                        />
                       }
-                    />
-                  }
-                  label={eventType}
-                />
-                {isUserAdmin(user?.username) && (
-                  <Button
-                    color="primary"
-                    disabled={
-                      event?.preparation.selectedEventType &&
-                      eventType !== event?.preparation.selectedEventType
-                    }
-                    onClick={() => {
-                      switchSelectedEventType(eventType);
-                    }}
-                  >
-                    {event?.preparation.selectedEventType === eventType
-                      ? 'Unselect'
-                      : 'Select'}
-                  </Button>
-                )}
-              </Grid>
-              {event?.preparation.eventTypeVotes
-                .filter((eventTypeVote) => eventTypeVote.type === eventType)
-                .sort((a, b) => a.username.localeCompare(b.username))
-                .map((eventTypeVote) => eventTypeVote.username)
-                .map((username) => (
-                  <Grid item key={`${eventType}-${username}`}>
-                    <Chip
-                      color="primary"
-                      label={username}
-                      variant={
-                        username === user?.username ? 'default' : 'outlined'
-                      }
+                      label={eventType}
                     />
                   </Grid>
-                ))}
-            </Grid>
-          ))}
-        {eventTypes &&
-          eventTypes.map((eventType) => (
-            <Grid item key={eventType} xs={12}></Grid>
-          ))}
+                  <Grid item>
+                    {isUserAdmin(user?.username) && (
+                      <Button
+                        color="primary"
+                        disabled={
+                          event?.preparation.selectedEventType &&
+                          eventType !== event?.preparation.selectedEventType
+                        }
+                        onClick={() => {
+                          switchSelectedEventType(eventType);
+                        }}
+                      >
+                        {event?.preparation.selectedEventType === eventType
+                          ? 'Unselect'
+                          : 'Select'}
+                      </Button>
+                    )}
+                  </Grid>
+                  {event?.preparation.eventTypeVotes
+                    .filter((eventTypeVote) => eventTypeVote.type === eventType)
+                    .sort((a, b) => a.username.localeCompare(b.username))
+                    .map((eventTypeVote) => eventTypeVote.username)
+                    .map((username) => (
+                      <Grid item key={`${eventType}-${username}`}>
+                        <Chip
+                          color="primary"
+                          label={username}
+                          variant={
+                            username === user?.username ? 'default' : 'outlined'
+                          }
+                        />
+                      </Grid>
+                    ))}
+                </Grid>
+              ))}
+          </Grid>
+          {eventTypes &&
+            eventTypes.map((eventType) => (
+              <Grid item key={eventType} xs={12}></Grid>
+            ))}
+        </Grid>
       </Grid>
       {isUserAdmin(user?.username) && (
         <ApplyFab
